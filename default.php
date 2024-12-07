0439f4d8907f8b1f633316498f1fbc600e68bcb2
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-07 13:54:32 +0800

    changes

--- default.php
+++ default.php
@@ -2,12 +2,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/default.css">
     <title>Document</title>
-    <style>
-        body {
-            background-color: red;
-        }
-    </style>
 </head>
 <body>
     <h1>TEST PUTANGINA</h1>
